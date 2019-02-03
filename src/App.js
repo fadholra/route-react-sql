ba4eed68737e9c054c9d653a2fc9399eb92e763f
Fadhol Robbaniy <fadhol.robbaniy.a@gmail.com>
2019-02-04 01:39:33 +0700

    update App

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+// default state for App constructor
+const stateDefault = {
+      no: '',
+      perjalanan: '',
+      noUpdate: 0,
+      startLat: '',
+      startLon: '',
+      endLat: '',
+      endLon: '',
+      wayPointNo: '',
+      wayPointLat: '',
+      wayPointLon: ''
+    }
+
+// main component
 class App extends Component {
   constructor(props) {
     super(props);
+    
     this.state = {
       datas: [{}],
-      /*data: {
-        no: 0, 
-        perjalanan: 'perjalanan'
-      },*/
-      no: '',
-      perjalanan: '',
-      noUpdate: 0
+      waypoints: [],
+      displayMap: []
     }
+    
+    // binding
     this.addPerjalanan = this.addPerjalanan.bind(this);
-    this.getPerjalanan = this.getPerjalanan.bind(this);
     this.editPerjalanan = this.editPerjalanan.bind(this);
     this.updatePerjalanan = this.updatePerjalanan.bind(this);
-    this.deletePerjalanan = this.deletePerjalanan.bind(this); 
-    this.handleChangeNo = this.handleChangeNo.bind(this);
-    this.handleChangePerjalanan = this.handleChangePerjalanan.bind(this);
+    this.deletePerjalanan = this.deletePerjalanan.bind(this);
   } 
 
   componentDidMount() {
@@ -29,9 +38,7 @@ class App extends Component {
   }
 
   addPerjalanan(e) {
-    //const { data } = this.state;
-    //e.preventDefault();
-    fetch(`http://localhost:4000/test/add?no=${this.state.no}&perjalanan=${this.state.perjalanan}`)
+    fetch(`http://localhost:4000/rute/add?no=${this.state.no}&perjalanan=${this.state.perjalanan}&start_lat=${this.state.startLat}&start_lon=${this.state.startLon}&end_lat=${this.state.endLat}&end_lon=${this.state.endLon}`)
       .then(this.getPerjalanan)
       .catch(err => err);
   }
@@ -42,75 +49,127 @@ class App extends Component {
     this.setState({
       no: data.no,
       perjalanan: data.perjalanan,
-      noUpdate: data.no
-    })
-    console.log(data, "data");
-    
+      noUpdate: data.no,
+      startLat: data.start_latitude,
+      startLon: data.start_longitude,
+      endLat: data.end_latitude,
+      endLon: data.end_longitude
+    })    
   }
 
   updatePerjalanan(e) {
     var noUpdate = parseInt(this.state.noUpdate);
     var no = parseInt(this.state.no);
-    fetch(`http://localhost:4000/test/update/${noUpdate}?no=${no}&perjalanan=${this.state.perjalanan}`)
+    fetch(`http://localhost:4000/rute/update/${noUpdate}?no=${no}&perjalanan=${this.state.perjalanan}&start_lat=${this.state.startLat}&start_lon=${this.state.startLon}&end_lat=${this.state.endLat}&end_lon=${this.state.endLon}`)
       .then(this.getPerjalanan)
       .catch(err => err);
   }
 
   deletePerjalanan(e) {
-    //e.preventDefault();
     var noDelete = e.target.value;
-    //console.log(noDelete, "no Delete");
-    fetch(`http://localhost:4000/test/delete/${noDelete}`)
+    fetch(`http://localhost:4000/rute/delete/${noDelete}`)
       .then(this.getPerjalanan)
       .catch(err => err);
   }
 
-  getPerjalanan() {
-    fetch('http://localhost:4000/test')
+  getPerjalanan = () => {
+    fetch('http://localhost:4000/rute')
       .then(response => response.json())
       .then(response => { 
         this.setState({ datas: response });
-        //console.log(this.state.data);
     });
+    fetch('http://localhost:4000/waypoints')
+      .then(response => response.json())
+      .then(response => { 
+        this.setState({ 
+          waypoints: response
+        });
+    });
+    this.setState(stateDefault);
+  }
+
+  // find specific data and parse to Map 
+  routePerjalanan = (e) => {
+    var data = this.state.datas.find(x => x.no == e.target.value);
+    var waypoints = this.state.waypoints.filter(x => x.no_rute == e.target.value);
+    var map = <Map startLat={ data.start_latitude } startLon={ data.start_longitude } endLat={ data.end_latitude } endLon={ data.end_longitude } waypoints={ waypoints } />;
     this.setState({
-      no: '',
-      perjalanan: '',
-      noUpdate: 0
+      displayMap: map
     })
   }
 
-  handleChangeNo(e) {
-    /*
-    this.setState(prevState => ({
-      data: {
-        ...prevState.data,
-        no: e.target.value
-      }
-    }))
-    */
+  handleChangeNo = (e) => {
     this.setState({
       no: e.target.value
     })
   }
 
-  handleChangePerjalanan(e) {
-    /*
-    this.setState(prevState => ({
-      data: {
-        ...prevState.data,
-        perjalanan: e.target.value
-      }
-    }))
-    */
+  handleChangePerjalanan = (e) => {
     this.setState({
       perjalanan: e.target.value
     })
   }
 
-  render() {
+  handleChangeStartLat = (e) => {
+    this.setState({
+      startLat: e.target.value
+    })
+  }
+  
+  handleChangeStartLon = (e) => {
+    this.setState({
+      startLon: e.target.value
+    })
+  }
+  
+  handleChangeEndLat = (e) => {
+    this.setState({
+      endLat: e.target.value
+    })
+  }
+  
+  handleChangeEndLon = (e) => {
+    this.setState({
+      endLon: e.target.value
+    })
+  }
 
+  // handle waypoint component
+  handleChangeWaypointNo = (e) => {
+    this.setState({
+      wayPointNo: e.target.value 
+    })
+  }
+
+  handleChangeWaypointLat = (e) => {
+    this.setState({
+      wayPointLat: e.target.value
+    })
+  }
+
+  handleChangeWaypointLon = (e) => {
+    this.setState({
+      wayPointLon: e.target.value
+    })
+  }
+
+  addWaypoint = (e) => {
+    var s = this.state;
+    fetch(`http://localhost:4000/waypoints/add?no_rute=${s.wayPointNo}&lat=${s.wayPointLat}&lon=${s.wayPointLon}`)
+      .then(this.getPerjalanan)
+      .catch(err => err);
+  }
+
+  deleteWaypoint = (e) => {
+    var idDelete = e.target.value;
+    fetch(`http://localhost:4000/waypoints/delete/${idDelete}`)
+      .then(this.getPerjalanan)
+      .catch(err => err);
+  }
+
+  render() {
     return (
-      <div className="App">
+      <div id="App">
         <h3>Rute Perjalanan</h3>   
         { this.state.datas.map(x => 
           <div className="table">
@@ -118,17 +177,133 @@ class App extends Component {
             <div className="perjalanan">{x.perjalanan}</div> 
             <button className="btn-delete" value={ x.no } onClick={ this.deletePerjalanan } >delete</button>
             <button className="btn-edit" value={ x.no } onClick={ this.editPerjalanan } >edit</button>
-            <button className="btn-route" >route</button>
-          </div>)}
-        <div className="input-add-update">
-          <input className="input-add-no" placeHolder="no..." value={ this.state.no } type="number" onChange={ this.handleChangeNo } />
-          <input className="input-add-perjalanan" placeHolder="kegiatan..." value={ this.state.perjalanan } onChange={ this.handleChangePerjalanan } />
+            <button className="btn-route" value={ x.no } onClick={ this.routePerjalanan }>route</button>
+          </div> )}
+        <div id="input-add-update">
+          <input className="input-add-no" placeholder="no" value={ this.state.no } type="number" onChange={ this.handleChangeNo } />
+          <input className="input-add-perjalanan" placeholder="kegiatan..." value={ this.state.perjalanan } onChange={ this.handleChangePerjalanan } />
           <button className="btn-add" onClick={ this.addPerjalanan } >add</button>
-          <button className="btn-update" onClick={ this.updatePerjalanan } >update</button>
+          <button className="btn-update" onClick={ this.updatePerjalanan } >update</button> <br />
+          <input className="input-add-start-lat" value={ this.state.startLat } onChange={ this.handleChangeStartLat } placeholder="start latitude..." />
+          <input className="input-add-start-lon" value={ this.state.startLon } onChange={ this.handleChangeStartLon } placeholder="start longitude..." /> <br />
+          <input className="input-add-end-lat" value={ this.state.endLat } onChange={ this.handleChangeEndLat } placeholder="end latitude..." />
+          <input className="input-add-end-lon" value={ this.state.endLon } onChange={ this.handleChangeEndLon } placeholder="end longitude..." />
         </div>
+        { this.state.displayMap }
+        <WayPoint no={ this.handleChangeWaypointNo } lat={ this.handleChangeWaypointLat } lon={ this.handleChangeWaypointLon } add={ this.addWaypoint } state={ this.state } />
+        { this.state.waypoints.map(x => 
+          <div className="table">
+            <div className="waypoint-no">{ x.no_rute }</div>
+            <div className="waypoint-lat">{ x.latitude }</div>
+            <div className="waypoint-lon">{ x.longitude }</div>
+            <button className="btn-delete-waypoint" value={ x.id } onClick={ this.deleteWaypoint } >delete</button>
+          </div> )}
       </div>
     );
   }
+}
+
+// add waypoint component
+class WayPoint extends Component {
+  render() {
+    //console.log(this.props.add, "add");
+    var props = this.props;
+    return (
+      <div id="waypoint">
+        <h3>Tambah WayPoint</h3>
+        <div className="waypoint-input">
+          <input className="waypoint-input-no" value={ props.state.wayPointNo } onChange={ props.no } type="number" placeholder="no rute" ></input>
+          <input className="waypoint-input-lat" value={ props.state.wayPointLat } onChange={ props.lat } placeholder="waypoint latitude..."></input>
+          <input className="waypoint-input-lon" value={ props.state.wayPointLon } onChange={ props.lon } placeholder="waypoint longitude..."></input>          
+        </div>
+        <button className="btn-add-waypoint" onClick={ props.add }>add</button>
+      </div>
+    )
+  }
+}
+
+// map component
+class Map extends Component {
+  componentDidMount() {    
+    this.renderMap();
+  }
+
+  // render maps
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyB6RKb0Y_vdn0lJc2DTDYZOsaipOxq4tYM&callback=initMap");
+    window.initMap = this.initMap;
+  }
+
+  // initiate map
+  initMap = () => {
+    var directionsService = new window.google.maps.DirectionsService;
+    var directionsDisplay = new window.google.maps.DirectionsRenderer;
+    var map = new window.google.maps.Map(document.getElementById('map'), {
+      //Sleman coords
+      center: {lat: -7.716165, lng: 110.335403},
+      zoom: 10
+    });
+
+    directionsDisplay.setMap(map);
+
+    // create waypoint
+    var waypts = [];
+    this.props.waypoints.map(x => 
+      waypts.push({
+        location: {
+          lat: parseFloat(x.latitude), 
+          lng: parseFloat(x.longitude)
+        }
+      })
+    );
+
+    // create start-end point
+    var startPoint = {
+      lat: parseFloat(this.props.startLat),
+      lng: parseFloat(this.props.startLon)
+    }
+
+    var endPoint = {
+      lat: parseFloat(this.props.endLat),
+      lng: parseFloat(this.props.endLon)
+    }
+
+    calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+    // show route on map
+    function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+      directionsService.route({
+        origin: startPoint,  
+        destination: endPoint,
+        waypoints: waypts,
+        optimizeWaypoints: true,
+        travelMode: 'DRIVING'
+      }, function(response, status) {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      });
+    }
+  }
+
+  render () {
+    
+    return (
+      <div id="map"></div>
+    ) 
+  }
+}
+
+// add new script
+function loadScript(source) {
+  var index = window.document.getElementsByTagName("script")[0]
+  var script = window.document.createElement("script")
+  script.src = source
+  script.async = true
+  script.defer = true
+  index.parentNode.insertBefore(script, index);
 }
 
 export default App;
